03782fca5057dc4138a024c2cef9968fbba0823f
nimtiaz-usc <143370753+nimtiaz-usc@users.noreply.github.com>
2025-02-13 18:49:55 -0800

    added some sounds and soundwave model

--- Assets/Scripts/IntroFadeOut.cs
+++ Assets/Scripts/IntroFadeOut.cs
@@ -7,8 +7,18 @@ using UnityEngine.UI;
 public class IntroFadeOut : MonoBehaviour
 {
     [SerializeField] float duration = 1f;
+    [SerializeField] AudioSource doorSFX;
+    [SerializeField] AudioSource lightSFX;
     void Start()
     {
-        GetComponent<Image>().DOFade(0, duration);
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade() {
+        doorSFX.Play();
+        yield return new WaitForSeconds(4f);
+        lightSFX.Play();
+        GetComponent<Image>().DOFade(0, duration).SetEase(Ease.OutQuart);
+        yield return null;
     }
 }
